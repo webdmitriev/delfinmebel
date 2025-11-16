20a89627450fd706f756b30072c9e7e0578250f2
webdmitriev <webdmitriev@gmail.com>
2025-11-16 11:15:39 +0300

    page-ourworks is ready

--- pages/page-ourworks.php
+++ pages/page-ourworks.php
@@ -13,144 +13,43 @@ $image_base64 = 'data:image/gif;base64,R0lGODlhBwAFAIAAAP///wAAACH5BAEAAAEALAAAA
   <div class="block-19">
     <div class="container df-fs-fs">
       <h1 class="h1">Мы гордимся нашими работами:</h1>
-      <div class="block-item df-sp-st w-100p">
-        <div class="block-item__content">
-          <h2 class="h2">Парты и стулья для общеобразовательной школы</h2>
-          <div class="block-wrap">
-            <div class="block-wrap__label icon-user">Директор</div>
-            <div class="block-wrap__descr">Космынина Лариса Николаевна</div>
-          </div>
-          <div class="block-wrap">
-            <div class="block-wrap__label icon-location">Город</div>
-            <div class="block-wrap__descr">Мамоново</div>
-          </div>
-        </div>
-        <div class="block-item__gallery">
-          <img src="<?= $url; ?>/assets/img/block-08/image-01.png" alt="alto" class="slide show-popup-gallery" />
-          <img src="<?= $url; ?>/assets/img/block-08/image-02.jpeg" alt="alto" class="slide show-popup-gallery" />
-          <img src="<?= $url; ?>/assets/img/block-08/image-03.jpeg" alt="alto" class="slide show-popup-gallery" />
-        </div>
-      </div>
-      <div class="block-item df-sp-st w-100p">
-        <div class="block-item__content">
-          <h2 class="h2">Парты и стулья для общеобразовательной школы</h2>
-          <div class="block-wrap">
-            <div class="block-wrap__label icon-user">Директор</div>
-            <div class="block-wrap__descr">Космынина Лариса Николаевна</div>
-          </div>
-          <div class="block-wrap">
-            <div class="block-wrap__label icon-location">Город</div>
-            <div class="block-wrap__descr">Мамоново</div>
-          </div>
-        </div>
-        <div class="block-item__gallery">
-          <img src="<?= $url; ?>/assets/img/block-08/image-01.png" alt="alto" class="slide show-popup-gallery" />
-          <img src="<?= $url; ?>/assets/img/block-08/image-02.jpeg" alt="alto" class="slide show-popup-gallery" />
-          <img src="<?= $url; ?>/assets/img/block-08/image-03.jpeg" alt="alto" class="slide show-popup-gallery" />
-        </div>
-      </div>
-      <div class="block-item df-sp-st w-100p">
-        <div class="block-item__content">
-          <h2 class="h2">Парты и стулья для общеобразовательной школы</h2>
-          <div class="block-wrap">
-            <div class="block-wrap__label icon-user">Директор</div>
-            <div class="block-wrap__descr">Космынина Лариса Николаевна</div>
-          </div>
-          <div class="block-wrap">
-            <div class="block-wrap__label icon-location">Город</div>
-            <div class="block-wrap__descr">Мамоново</div>
-          </div>
-        </div>
-        <div class="block-item__gallery">
-          <img src="<?= $url; ?>/assets/img/block-08/image-01.png" alt="alto" class="slide show-popup-gallery" />
-          <img src="<?= $url; ?>/assets/img/block-08/image-02.jpeg" alt="alto" class="slide show-popup-gallery" />
-          <img src="<?= $url; ?>/assets/img/block-08/image-03.jpeg" alt="alto" class="slide show-popup-gallery" />
-        </div>
-      </div>
-      <div class="block-item df-sp-st w-100p">
-        <div class="block-item__content">
-          <h2 class="h2">Парты и стулья для общеобразовательной школы</h2>
-          <div class="block-wrap">
-            <div class="block-wrap__label icon-user">Директор</div>
-            <div class="block-wrap__descr">Космынина Лариса Николаевна</div>
-          </div>
-          <div class="block-wrap">
-            <div class="block-wrap__label icon-location">Город</div>
-            <div class="block-wrap__descr">Мамоново</div>
-          </div>
-        </div>
-        <div class="block-item__gallery">
-          <img src="<?= $url; ?>/assets/img/block-08/image-01.png" alt="alto" class="slide show-popup-gallery" />
-          <img src="<?= $url; ?>/assets/img/block-08/image-02.jpeg" alt="alto" class="slide show-popup-gallery" />
-          <img src="<?= $url; ?>/assets/img/block-08/image-03.jpeg" alt="alto" class="slide show-popup-gallery" />
-        </div>
-      </div>
-      <div class="block-item df-sp-st w-100p">
-        <div class="block-item__content">
-          <h2 class="h2">Парты и стулья для общеобразовательной школы</h2>
-          <div class="block-wrap">
-            <div class="block-wrap__label icon-user">Директор</div>
-            <div class="block-wrap__descr">Космынина Лариса Николаевна</div>
-          </div>
-          <div class="block-wrap">
-            <div class="block-wrap__label icon-location">Город</div>
-            <div class="block-wrap__descr">Мамоново</div>
-          </div>
-        </div>
-        <div class="block-item__gallery">
-          <img src="<?= $url; ?>/assets/img/block-08/image-01.png" alt="alto" class="slide show-popup-gallery" />
-          <img src="<?= $url; ?>/assets/img/block-08/image-02.jpeg" alt="alto" class="slide show-popup-gallery" />
-          <img src="<?= $url; ?>/assets/img/block-08/image-03.jpeg" alt="alto" class="slide show-popup-gallery" />
-        </div>
-      </div>
-    </div>
-  </div>
+      <?php
 
-  <div class="block-14">
-    <div class="container">
-      <img src="<?= $url; ?>/assets/img/block-14/bg-image-01.webp" alt="alto" class="block-bg-01" />
-      <img src="<?= $url; ?>/assets/img/block-14/bg-image-02.svg" alt="alto" class="block-bg-02" />
-      <div class="block-wrap">
-        <h2 class="h2">Подписывайтесь на наше сообщество ВКонтакте и узнавайте все новости первыми</h2>
-        <div class="descr">Здесь мы выкладываем наши работы в образовательных учреждениях Калининграда и области</div>
-        <a href="#" target="_blank" rel="noopener noreferrer" class="link-vk">/school_mebel39</a>
-      </div>
-    </div>
-  </div>
+        $args = array(
+          'post_type'      => 'works',
+          'posts_per_page' => -1,
+        );
 
-  <div class="block-15">
-    <div class="container df-sp-st">
-      <h2 class="h2">Контакты</h2>
-      <div class="block-item df-sp-ce w-100p">
-        <div class="block-item__icon df-ce-ce"><img src="<?= $url; ?>/assets/img/block-15/icon-01.svg" alt="alto" /></div>
-        <div class="block-item__content">
-          <div class="block-item__label">label</div>
-          <div class="block-item__descr">descr</div>
-        </div>
-      </div>
-      <div class="block-item df-sp-ce w-100p">
-        <div class="block-item__icon df-ce-ce"><img src="<?= $url; ?>/assets/img/block-15/icon-02.svg" alt="alto" /></div>
-        <div class="block-item__content">
-          <div class="block-item__label">label</div>
-          <div class="block-item__descr">descr</div>
-        </div>
-      </div>
-      <div class="block-item df-sp-ce w-100p">
-        <div class="block-item__icon df-ce-ce"><img src="<?= $url; ?>/assets/img/block-15/icon-03.svg" alt="alto" /></div>
-        <div class="block-item__content">
-          <div class="block-item__label">label</div>
-          <div class="block-item__descr">descr</div>
-        </div>
-      </div>
-      <div class="block-item df-sp-ce w-100p">
-        <div class="block-item__icon df-ce-ce"><img src="<?= $url; ?>/assets/img/block-15/icon-04.svg" alt="alto" /></div>
-        <div class="block-item__content">
-          <div class="block-item__label">label</div>
-          <div class="block-item__descr">descr</div>
-        </div>
-      </div>
+        $works_query = new WP_Query($args);
+        if ($works_query->have_posts()): while ($works_query->have_posts()): $works_query->the_post();
+          $director = get_post_meta(get_the_ID(), 'director', true);
+          $city = get_post_meta(get_the_ID(), 'city', true);
+          $gallery = get_post_meta(get_the_ID(), 'gallery', true);
+        ?>
+          <div class="block-item df-sp-st w-100p">
+            <div class="block-item__content">
+              <h2 class="h2"><?php the_title(); ?></h2>
+              <div class="block-wrap">
+                <div class="block-wrap__label icon-user">Директор</div>
+                <?php if($director): ?><div class="block-wrap__descr"><?php echo esc_html($director); ?></div><?php endif; ?>
+              </div>
+              <div class="block-wrap">
+                <div class="block-wrap__label icon-location">Город</div>
+                <?php if($city): ?><div class="block-wrap__descr"><?php echo esc_html($city); ?></div><?php endif; ?>
+              </div>
+            </div>
+            <div class="block-item__gallery">
+              <?php if($gallery): foreach ($gallery as $key => $value) { ?>
+                <img src="<?= $value; ?>" alt="alto" class="slide show-popup-gallery" />
+              <?php } endif; ?>
+            </div>
+          </div>
+        <?php endwhile; wp_reset_postdata(); else : ?>
+          <p>Наши работы не наполнены.</p>
+        <?php endif; ?>
     </div>
   </div>
 
 <?php
+the_content();
 get_footer();
